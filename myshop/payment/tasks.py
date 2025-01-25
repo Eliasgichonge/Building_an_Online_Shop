4d69c7e3b1242a3e3ad3462fc92fd282e1b38eeb
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-01-26 00:18:39 +0300

    Building the coupon model

--- myshop/payment/tasks.py
+++ myshop/payment/tasks.py
@@ -1,6 +1,6 @@
 from io import BytesIO
 from celery import shared_task
-import weasyprint
+# import weasyprint
 from django.template.loader import render_to_string
 from django.core.mail import EmailMessage
 from django.conf import settings
