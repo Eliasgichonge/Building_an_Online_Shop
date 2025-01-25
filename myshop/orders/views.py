467c474592329265ef62dd2aecd97ea1d632640a
Elias Mang'era Mwita <eliasmwita86@gmail.com>
2025-01-25 21:34:18 +0300

    Building the payment process

--- myshop/orders/views.py
+++ myshop/orders/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.urls import reverse
+from django.shortcuts import render, redirect
 from .models import OrderItem
 from .forms import OrderCreateForm
 from .tasks import order_created
@@ -20,6 +21,8 @@ def order_create(request):
             cart.clear()
             # launch asynchronous task
             order_created.delay(order.id)
+            # set the order in the session
+            request.session['order_id'] = order.id
             return render(request,
                           'orders/order/created.html',
                           {'order': order})
